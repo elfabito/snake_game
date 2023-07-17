bf5c0bcadca84a748f0f342f5f84d1d18be7832c
elfabito <elfabito@gmail.com>
2023-07-17 17:58:36 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+from turtle import Turtle, Screen
+from snake import Snake
+from food import Food
+import time
+from scoreboard import ScoreBoard
+
+
+
+s = Screen()
+s.setup(width=600, height=600)
+s.bgcolor("black")
+s.title("La viborita")
+s.tracer(0)
+
+snake = Snake()
+food = Food()
+score = ScoreBoard()
+s.listen()
+s.onkey(snake.up, "Up")
+s.onkey(snake.down, "Down")
+s.onkey(snake.left, "Left")
+s.onkey(snake.right, "Right")
+
+
+game_is_on = True
+while game_is_on:
+
+    s.update()
+    time.sleep(0.1)
+
+    snake.move()
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        score.raise_score()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+
+        game_is_on = False
+        score.game_over()
+    for elem in snake.list[1:]:
+
+        if snake.head.distance(elem) < 10:
+            game_is_on = False
+            score.game_over()
+s.exitonclick()
---
+++ snake.py
@@ -0,0 +1,62 @@
+from turtle import Turtle
+POSS = [(0, 0), (-20, 0), (-40, 0)]
+HEADING = [ 0, 90, 180, 270]
+MOVE_DIS = 20
+class Snake:
+
+    def __init__(self):
+        self.list = []
+        self.create_snake()
+        self.head = self.list[0]
+    # def __init__(self):
+    #     self.list = []
+    #
+    #     poss = [(0, 0), (-20, 0), (-40, 0)]
+    #     for position in poss:
+    #         self.t = Turtle("square")
+    #         self.t.color("white")
+    #         self.t.penup()
+    #         self.t.goto(position)
+    #         self.list.append(self.t)
+    #
+    def create_snake(self):
+        for position in POSS:
+            self.add_seg(position)
+
+    def add_seg(self, position):
+        t = Turtle("square")
+        t.color("white")
+        t.penup()
+        t.goto(position)
+        self.list.append(t)
+
+    def extend(self):
+
+        self.add_seg(self.list[-1].position())
+
+    def move(self):
+        for poss_index in range(len(self.list) - 1, 0, -1):
+            new_x = self.list[poss_index - 1].xcor()
+            new_y = self.list[poss_index - 1].ycor()
+            self.list[poss_index].goto(new_x, new_y)
+        self.head.forward(MOVE_DIS)
+
+    def up(self):
+        if self.head.heading() != 270:
+            self.head.setheading(90)
+
+    def down(self):
+        if self.head.heading() != 90:
+            self.head.setheading(270)
+
+    def left(self):
+        if self.head.heading() != 0:
+            self.head.setheading(180)
+
+    def right(self):
+        if self.head.heading() != 180:
+            self.head.setheading(0)
+
+
+
+
